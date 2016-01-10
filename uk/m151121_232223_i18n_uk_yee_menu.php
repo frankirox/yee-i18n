149540810aeed6551360db8ada2e1f01f3bcc7b4
YeeSoft <makitrataras@gmail.com>
2016-01-10 21:23:52 +0200

    update 0.0.1-rc

--- uk/m151121_232223_i18n_uk_yee_menu.php
+++ uk/m151121_232223_i18n_uk_yee_menu.php
@@ -27,6 +27,12 @@ class m151121_232223_i18n_uk_yee_menu extends TranslatedMessagesMigration
             'Create Menu Link' => 'Створити Поcилання',
             'Update Menu Link' => 'Оновити Поcилання',
             'Menu Links' => 'Поcилання',
+            'Add New Menu' => 'Додати Меню',
+            'Add New Link' => 'Додати Поcилання',
+            'An error occurred during saving menu!' => 'Виникла помилка під час збереження меню!',
+            'The changes have been saved.' => 'Зміни були успішно збережені.',
+            'Please, select menu to view menu links...' => 'Будь ласка, виберіть меню, щоб переглянути посилання...',
+            'Selected menu doesn\'t contain any link. Click "Add New Link" to create a link for this menu.' => 'Вибране меню не містить жодного посилання. Натисніть кнопку "Додати Поcилання", щоб створити нове посилання для цього меню.',
         ];
     }
 }
