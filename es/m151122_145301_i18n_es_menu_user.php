1dd773c3d974c866722511935cdd6789a8daf5af
Ignacio Salcedo <ignacio@isalcedo.com>
2017-11-05 10:05:39 -0500

    Small Fix

--- es/m151122_145301_i18n_es_menu_user.php
+++ es/m151122_145301_i18n_es_menu_user.php
@@ -7,12 +7,12 @@ class m151122_145301_i18n_es_menu_user extends Migration
 
     public function up()
     {
-        $this->insert('{{%menu_link_lang}}', ['link_id' => 'user', 'label' => 'Usuarios', 'language' => 'uk']);
-        $this->insert('{{%menu_link_lang}}', ['link_id' => 'user-groups', 'label' => 'Grupos de permisos', 'language' => 'uk']);
-        $this->insert('{{%menu_link_lang}}', ['link_id' => 'user-log', 'label' => 'Registro de visitas', 'language' => 'uk']);
-        $this->insert('{{%menu_link_lang}}', ['link_id' => 'user-permission', 'label' => 'Permisos', 'language' => 'uk']);
-        $this->insert('{{%menu_link_lang}}', ['link_id' => 'user-role', 'label' => 'Roles', 'language' => 'uk']);
-        $this->insert('{{%menu_link_lang}}', ['link_id' => 'user-user', 'label' => 'Usuarios', 'language' => 'uk']);
+        $this->insert('{{%menu_link_lang}}', ['link_id' => 'user', 'label' => 'Usuarios', 'language' => 'es']);
+        $this->insert('{{%menu_link_lang}}', ['link_id' => 'user-groups', 'label' => 'Grupos de permisos', 'language' => 'es']);
+        $this->insert('{{%menu_link_lang}}', ['link_id' => 'user-log', 'label' => 'Registro de visitas', 'language' => 'es']);
+        $this->insert('{{%menu_link_lang}}', ['link_id' => 'user-permission', 'label' => 'Permisos', 'language' => 'es']);
+        $this->insert('{{%menu_link_lang}}', ['link_id' => 'user-role', 'label' => 'Roles', 'language' => 'es']);
+        $this->insert('{{%menu_link_lang}}', ['link_id' => 'user-user', 'label' => 'Usuarios', 'language' => 'es']);
     }
 
 }
